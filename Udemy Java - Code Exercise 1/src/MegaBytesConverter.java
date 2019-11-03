24776e95255a930375fe75dc009b09403f03cf14
Nick Njoku <nnjoku245@ou.edu>
2019-11-03 06:29:06 -0600

    Added comments to MegaBytesConverter class

--- Udemy Java - Code Exercise 1/src/MegaBytesConverter.java
+++ Udemy Java - Code Exercise 1/src/MegaBytesConverter.java
@@ -1,13 +1,25 @@
-
+/**
+ * Class that has one method with the purpose of converting KBs to MBs
+ * @author nicholasnjoku
+ *
+ */
 public class MegaBytesConverter {
 	
+	/**
+	 * Takes in # of KBs and prints out the conversion to MB
+	 * 1 MB = 1024 KB
+	 * 
+	 * @param kiloBytes
+	 */
 	public static void printMegaBytesAndKiloBytes(int kiloBytes)
 	{
+		// Covers the case if the input kiloBytes is negative
 		if(kiloBytes <0)
 		{
 			System.out.println("Invalid Value");
 		}
 		
+		// Conversion (1MB = 1024KB)
 		else
 		{
 			int totalMB = kiloBytes / 1024;
